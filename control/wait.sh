0ccbe9b6644861c248950a3503aad446b7242040
Chris Adeniyi-Jones <chris.adeniyi-jones@arm.com>
2025-07-10 16:27:16 +0100

    Simulation on by default. Controlled by env var

--- control/wait.sh
+++ control/wait.sh
@@ -1,5 +1,21 @@
 #!/bin/sh -fvx
 
+
+HOST="mosquitto"
+PORT=1883
+
+echo "Waiting for MQTT broker at $HOST:$PORT..."
+
+while ! nc -z "$HOST" "$PORT" >/dev/null 2>&1; do
+    echo "MQTT broker not available yet. Retrying in 2 seconds..."
+    sleep 2
+done
+
+echo "MQTT broker is now running at $HOST:$PORT."
+
+sleep 2
+
+
 while true; do
 
     status=$(curl -s -o /dev/null -w "%{http_code}" http://homeassistant:8123/api/)
@@ -14,15 +30,17 @@ while true; do
     sleep 5
 done
 
-mosquitto_sub -h mosquitto -p 1883 -t '#' -C 1
-
-sleep 2
 
 SCRIPT_DIR=$(dirname "$0")
 
 $SCRIPT_DIR/config.py
 
-MQTT_BROKER=mosquitto MQTT_PORT=1883 $SCRIPT_DIR/external_simulate.py --interval 10 --temperature --motion --presence > /var/log/mylog.log 2>&1 &
+if [ ! -z "$SIMULATE" ]; then 
+  MQTT_BROKER=mosquitto MQTT_PORT=1883 $SCRIPT_DIR/change_lights.py /var/log/light.log 2>&1 &
+  MQTT_BROKER=mosquitto MQTT_PORT=1883 $SCRIPT_DIR/external_simulate.py --interval 10 --temperature --motion --presence > /var/log/sim.log 2>&1 &
+  MQTT_BROKER=mosquitto MQTT_PORT=1883 $SCRIPT_DIR/rotate_camera  > /var/log/cam.log 2>&1 &
+fi
 
-MQTT_BROKER=mosquitto MQTT_PORT=1883 $SCRIPT_DIR/change_lights.py
+tail -f /dev/null
 
+                      
